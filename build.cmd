f25f171a82fa6496d42f1a6cb6577fc49e985bbd
Vagif Abilov <vagif.abilov@nrk.no>
2023-05-03 15:43:42 +0200

    Removed .cmd script files.

--- build.cmd
+++
@@ -1,1 +0,0 @@
-dotnet build Akka.Persistence.Reminders.sln --configuration Release -p:VersionPrefix=%1
--- pack.cmd
+++
@@ -1,1 +0,0 @@
-dotnet pack Akka.Persistence.Reminders.sln --configuration Release -p:VersionPrefix=%1 --output ./build/packages
